91ec0e8a7928e9594a44a5fdf9348db71dc53ea3
JZZ <zheng1796806850@gmail.com>
2026-01-17 22:51:31 -0500

    Basic player movement and 2.5D perspective
    1. Implemented basic player movement and animations (using test assets)
    2. Implemented a 2.5D top-down perspective similar to Don't Starve

---
+++ Assets/Script/PlayerMovementController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private float inputX, inputY;
+    private float stopX, stopY;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = Camera.main.transform.position - transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        inputX = Input.GetAxisRaw("Horizontal");
+        inputY = Input.GetAxisRaw("Vertical");
+        Vector2 input = new Vector2(inputX, inputY).normalized;
+        rb.linearVelocity = input * speed;
+
+        if (input != Vector2.zero)
+        {
+            animator.SetBool("isMoving", true);
+            stopX = inputX;
+            stopY = inputY;
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+        animator.SetFloat("InputX", stopX);
+        animator.SetFloat("InputY", stopY);
+
+        Camera.main.transform.position = transform.position + offset;
+    }
+}
